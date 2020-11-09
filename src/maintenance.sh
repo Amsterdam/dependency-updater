d6674c02d9cbb8b7c3f34dd206dca1ee7073c82d
Niels Lensink <nielslensink@gmail.com>
2020-11-09 10:02:44 +0100

    Added iiif and waarnemingen-mensen

--- src/maintenance.sh
+++ src/maintenance.sh
@@ -5,8 +5,10 @@ echo "$BRANCH"
 declare -A array
 array[bed-and-breakfast]=git@git.data.amsterdam.nl:Datapunt/bed-and-breakfast.git
 array[iiif-metadata-server]=git@github.com:Amsterdam/iiif-metadata-server.git
+array[iiif-auth-proxy]=git@github.com:Amsterdam/iiif-auth-proxy.git
 array[waarnemingen-voertuigen]=git@github.com:Amsterdam/waarnemingen-voertuigen.git
 array[waarnemingen-dashboard]=git@github.com:Amsterdam/waarnemingen-dashboard.git
+array[waarnemingen-mensen]=git@github.com:Amsterdam/waarnemingen-mensen.git
 array[waarnemingen-boten]=git@github.com:Amsterdam/waarnemingen-boten.git
 array[kwiz-schuldhulpverlenings-monitor]=git@git.data.amsterdam.nl:Datapunt/kwiz-schuldhulpverlenings-monitor.git
 array[tellus]=git@github.com:Amsterdam/tellus.git
